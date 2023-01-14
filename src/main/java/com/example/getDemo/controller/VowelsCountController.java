35876729a8fb56f38dc5e71235d1a7b05ad0b3b5
shreya-albanero <shreya.neema@albanero.io>
2023-01-14 19:16:54 +0530

    Using @RequestParam

--- src/main/java/com/example/getDemo/controller/VowelsCountController.java
+++ src/main/java/com/example/getDemo/controller/VowelsCountController.java
@@ -3,25 +3,24 @@ package com.example.getDemo.controller;
 import com.example.getDemo.entity.VowelsCount;
 import com.example.getDemo.repository.VowelsCountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class VowelsCountController {
     @Autowired
     VowelsCountRepo vowelsCountRepo;
 
-    @GetMapping("/getString/{string}")
-    public int countVowels(@PathVariable String string){
-        String s=string;
+    @GetMapping("/getString")
+    public int countVowels(@RequestParam("string") String string){
         int count=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='a' ||s.charAt(i) == 'e'|| s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i)=='/' ||s.charAt(i)=='$' || s.charAt(i)=='@' || s.charAt(i)=='#') {
+        for(int i=0; i<string.length(); i++){
+            if(string.charAt(i)=='a' ||string.charAt(i) == 'e'|| string.charAt(i) == 'i' || string.charAt(i) == 'o' || string.charAt(i) == 'u' || string.charAt(i)=='/' ||string.charAt(i)=='$' || string.charAt(i)=='@' || string.charAt(i)=='#') {
                 count++;
             }
         }
+        //m.addAttribute("message", m);
         System.out.println(count);
         return count;
     }
